bf7dc3271c94a78287fdcac6efaeb9d4d7bb2a2f
Prem <premprakash0796@gmail.com>
2017-10-28 02:26:38 +0530

    Create methods Building URL and for Http respose within NetworkUtils Class

---
+++ app/src/main/java/utils/Constants.java
@@ -0,0 +1,13 @@
+package utils;
+
+/**
+ * Created by Prem on 28-10-2017.
+ * This is the class which holds all the constants used in this project.
+ */
+
+public class Constants {
+
+    //Base URL
+    public static final String BAKING_BASE_URL = "http://go.udacity.com";
+    public static final String BAKING_SUPPORT_URL = "android-baking-app-json";
+}
--- app/src/main/java/utils/NetworkUtils.java
+++ app/src/main/java/utils/NetworkUtils.java
@@ -1,10 +1,66 @@
 package utils;
 
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
 /**
  * Created by Prem on 28-10-2017.
  * This is a Utils class that handles operation related to networking and to fetch information
  */
 
 public class NetworkUtils {
+
+    //Build the URL to query the data
+    ///http://go.udacity.com/android-baking-app-json
+
+    public static URL buildURL() {
+
+        Uri buildUrl = Uri.parse(Constants.BAKING_BASE_URL).buildUpon()
+                .appendEncodedPath(Constants.BAKING_SUPPORT_URL)
+                .build();
+        return convertAndroidUrltoJavaUrl(buildUrl);
+    }
+
+    private static URL convertAndroidUrltoJavaUrl(Uri uriToConvert) {
+        URL url = null;
+        try {
+            url = new URL(uriToConvert.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    /**
+     * This method returns the entire result from the Http response
+     * throws IOException related to network and stream reading
+     * params url The URL to fetch the HTTP response from.
+     * returns The contents of Http response
+     */
+
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
 
 }
