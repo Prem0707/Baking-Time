7b3beb81d798d1ccd793d4da8f0a1f7d5c0d4f45
Prem <premprakash0796@gmail.com>
2017-10-28 02:05:52 +0530

    Create a utils package which contains NetworkUtils Class to handle networking

--- app/src/main/java/com/prem/android/bakingtime/activities/MainActivity.java
+++ app/src/main/java/com/prem/android/bakingtime/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.prem.android.bakingtime;
+package com.prem.android.bakingtime.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.prem.android.bakingtime.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/utils/NetworkUtils.java
@@ -0,0 +1,10 @@
+package utils;
+
+/**
+ * Created by Prem on 28-10-2017.
+ * This is a Utils class that handles operation related to networking and to fetch information
+ */
+
+public class NetworkUtils {
+
+}
