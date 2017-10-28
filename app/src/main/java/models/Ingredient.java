44ec0ff432605c9a7b2c5172bedfb2a60d520abf
Prem <premprakash0796@gmail.com>
2017-10-28 09:24:50 +0530

    define data model for Recipe and define class for Ingredient and Step

---
+++ app/src/main/java/models/Ingredient.java
@@ -0,0 +1,10 @@
+package models;
+
+/**
+ * Created by Prem on 28-10-2017.
+ */
+
+class Ingredient {
+    
+
+}
--- app/src/main/java/models/Recipe.java
+++ app/src/main/java/models/Recipe.java
@@ -1,9 +1,71 @@
 package models;
 
+import java.util.ArrayList;
+
 /**
  * Created by Prem on 28-10-2017.
  */
 
 public class Recipe {
-    
+
+    private int id;
+    private String name;
+    private String image;
+    private ArrayList<Ingredient> ingredients;
+    private ArrayList<Step> steps;
+
+
+
+    public Recipe(int id, String name, String image, ArrayList<Ingredient> ingredients, ArrayList<Step> steps) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.ingredients = ingredients;
+        this.steps = steps;
+    }
+
+    public Recipe(){
+
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public ArrayList<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(ArrayList<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public ArrayList<Step> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(ArrayList<Step> steps) {
+        this.steps = steps;
+    }
 }
