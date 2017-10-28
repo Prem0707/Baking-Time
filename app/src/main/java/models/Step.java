7cabf48915eacd4702ec3096b99112cf812676af
Prem <premprakash0796@gmail.com>
2017-10-28 09:36:55 +0530

    define data model for Step class

--- app/src/main/java/models/Step.java
+++ app/src/main/java/models/Step.java
@@ -6,4 +6,31 @@ package models;
 
 class Step {
 
+    private int i;d
+    private String shortDescription;
+    private String description;
+    private String videoURL;
+    private String thumbnailURL;
+
+
+    public Step(int i, String shortDescription, String description, String videoURL, String thumbnailURL) {
+        this.i = i;
+        this.shortDescription = shortDescription;
+        this.description = description;
+        this.videoURL = videoURL;
+        this.thumbnailURL = thumbnailURL;
+    }
+
+
+    public Step(){
+
+    }
+
+    public String getVideoURL() {
+        return videoURL;
+    }
+
+    public void setVideoURL(String videoURL) {
+        this.videoURL = videoURL;
+    }
 }
