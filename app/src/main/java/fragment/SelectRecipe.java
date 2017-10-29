5ab673551665007610b3a042c6785be1cf890284
Prem <premprakash0796@gmail.com>
2017-10-29 06:19:51 +0530

    use ButterKnife library to acess views in SelectRecipe

--- app/src/main/java/fragment/SelectRecipe.java
+++ app/src/main/java/fragment/SelectRecipe.java
@@ -3,6 +3,8 @@ package fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +13,8 @@ import com.prem.android.bakingtime.R;
 
 import java.util.ArrayList;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import interfaces.TaskCompleted;
 import models.Recipe;
 
@@ -21,16 +25,20 @@ public class SelectRecipe extends Fragment implements TaskCompleted{
 
 
     ArrayList<Recipe> recipeList;
-    public SelectRecipe() {
-        // Required empty public constructor
-    }
 
+    // required Constructor
+    public SelectRecipe() { }
+
+    @BindView(R.id.recipe_recyclerview) RecyclerView mRecyclerView;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_select_recipe, container, false);
+        View view = inflater.inflate(R.layout.fragment_select_recipe, container, false);
+        ButterKnife.bind(this, view);
+        mRecyclerView = new RecyclerView(getContext());
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        return view;
     }
 
      //Called when the Fragment is visible to the user
