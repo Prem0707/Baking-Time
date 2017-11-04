bd5c7da02436a8b6f0680dd036c4cdde009058ca
Prem <premprakash0796@gmail.com>
2017-11-04 08:05:16 +0530

    ExoPlayer: create the default Track Selecter, exoplayer instance and bind player to the view

--- app/src/main/java/fragment/StepsDetailFragment.java
+++ app/src/main/java/fragment/StepsDetailFragment.java
@@ -2,19 +2,24 @@ package fragment;
 
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
 import com.prem.android.bakingtime.R;
-
-import static com.prem.android.bakingtime.R.id.playerView;
 
 /**
  * This fragment will contain the videos of steps along with detailed Step of recipe preparation .
@@ -50,33 +55,27 @@ public class StepsDetailFragment extends Fragment {
         mDetailedTextView = (TextView) view.findViewById(R.id.detailed_description);
         mDetailedTextView.setText(mDescription);
 
-        //Initialise the Player view;
-        mPlayerview = (SimpleExoPlayerView) view.findViewById(playerView);
-        //Initialise the Player
-        //initialisePlayer(Uri.parse(mvideoURL));
+        if(!mVideoURL.isEmpty()){
+            setupExoPlayer();
+        }
         return view;
     }
 
-    /**
-     * Initialize Exoplayer
-     *
-     * @param  mediaUri: the uri of sample to play
-     */
-    private void initialisePlayer(Uri mediaUri){
-     if(mExoPlayer != null){
-         //Create an instance of ExoPlayer
-        // TrackSelector trackSelector = new DefaultTrackSelector();
-        // LoadControl loadControl = new DefaultLoadControl();
-        // mExoPlayer = ExoPlayerFactory.newSimpleInstance(context,trackSelector, loadControl );
-        // mPlayerview.setPlayer(mExoPlayer);
+    private void setupExoPlayer(){
 
-         //Prepare the media Source
-        // String userAgent = Util.getUserAgent(context, "Baking Time");
-        // MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(context, userAgent),
-        //         new DefaultExtractorsFactory(), null, null);
-        // mExoPlayer.prepare(mediaSource);
-        // mExoPlayer.setPlayWhenReady(true);
+        // 1. Create a default TrackSelector
+        Handler mainHandler = new Handler();
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector =
+                new DefaultTrackSelector(videoTrackSelectionFactory);
 
-     }
+         // 2. Create the player
+        SimpleExoPlayer player =
+                ExoPlayerFactory.newSimpleInstance(context, trackSelector);
+
+        // Bind the player to the view.
+        mPlayerview.setPlayer(player);
     }
 }
