6bf91931fcfedf829137c8d916328c0c2a373711
Prem <premprakash0796@gmail.com>
2017-11-04 09:07:24 +0530

    create the MediaSource to play, pass that object to ExoPlayer.prepare and setPlayWhenReady can be used to start and pause playback

--- app/src/main/java/fragment/StepsDetailFragment.java
+++ app/src/main/java/fragment/StepsDetailFragment.java
@@ -2,6 +2,7 @@ package fragment;
 
 
 import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
@@ -12,13 +13,20 @@ import android.widget.TextView;
 
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
 import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
 import com.google.android.exoplayer2.trackselection.TrackSelection;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
 import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.prem.android.bakingtime.R;
 
 /**
@@ -65,6 +73,8 @@ public class StepsDetailFragment extends Fragment {
 
         // 1. Create a default TrackSelector
         Handler mainHandler = new Handler();
+
+        // Measures bandwidth during playback. Can be null if not required.
         BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
         TrackSelection.Factory videoTrackSelectionFactory =
                 new AdaptiveTrackSelection.Factory(bandwidthMeter);
@@ -77,5 +87,22 @@ public class StepsDetailFragment extends Fragment {
 
         // Bind the player to the view.
         mPlayerview.setPlayer(player);
+
+        // Measures bandwidth during playback. Can be null if not required.
+        DefaultBandwidthMeter bandwidthMeasure = new DefaultBandwidthMeter();
+        // Produces DataSource instances through which media data is loaded.
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(context,
+                Util.getUserAgent(context, "Baking Time"), bandwidthMeasure);
+        // Produces Extractor instances for parsing the media data.
+        ExtractorsFactory extractorsFactory = new DefaultExtractorsFactory();
+
+        // This is the MediaSource representing the media to be played.
+        MediaSource videoSource = new ExtractorMediaSource(Uri.parse(mVideoURL),
+                dataSourceFactory, extractorsFactory, null, null);
+         // Prepare the player with the source.
+        player.prepare(videoSource);
+
+        //setPlayWhenReady can be used to start and pause playback
+        player.setPlayWhenReady(true);
     }
 }
