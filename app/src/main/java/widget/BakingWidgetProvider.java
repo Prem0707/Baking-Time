e4eb1b075203118f1fe33a0065e21236ac8622d3
Prem <premprakash0796@gmail.com>
2017-12-18 18:08:22 +0530

    define layout file for widget

--- app/src/main/java/widget/BakingWidgetProvider.java
+++ app/src/main/java/widget/BakingWidgetProvider.java
@@ -1,11 +1,14 @@
 package widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 import com.prem.android.bakingtime.R;
+import com.prem.android.bakingtime.activities.IngredientActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -15,10 +18,15 @@ public class BakingWidgetProvider extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baking_widget_provider);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        //Create an Intent to launch recipeIngredient when clicked
+        Intent intent = new Intent(context, IngredientActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //widgets allows only clickHandlers to launch only pending Intent
+        views.setOnClickPendingIntent(R.id.layout_containing_widget, pendingIntent);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
