7c6fd58a900ec2b92e492b4b6b3df7348d19a1d1
Prem <premprakash0796@gmail.com>
2017-12-19 17:34:03 +0530

    Add a simple widget by Android Studio

--- app/src/main/java/widget/IngredientListWidget.java
+++ app/src/main/java/widget/IngredientListWidget.java
@@ -1,32 +1,24 @@
 package widget;
 
-import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
-import android.content.Intent;
 import android.widget.RemoteViews;
 
 import com.prem.android.bakingtime.R;
-import com.prem.android.bakingtime.activities.IngredientActivity;
 
 /**
  * Implementation of App Widget functionality.
  */
-public class BakingWidgetProvider extends AppWidgetProvider {
+public class IngredientListWidget extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
 
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.baking_widget_provider);
-
-        //Create an Intent to launch recipeIngredient when clicked
-        Intent intent = new Intent(context, IngredientActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-        //widgets allows only clickHandlers to launch only pending Intent
-        views.setOnClickPendingIntent(R.id.layout_containing_widget, pendingIntent);
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.ingredient_list_widget);
+        views.setTextViewText(R.id.appwidget_text, widgetText);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
